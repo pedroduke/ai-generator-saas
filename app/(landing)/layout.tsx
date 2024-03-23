f01739d5959e9a1a84a0f455d6e3c4ca02e82ae9
Pedro Duque <pedroduque8@gmail.com>
2024-03-23 15:21:48 +0100

    update readme

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,8 +1,13 @@
+import StickyBar from '@/components/sticky-bar';
+
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='h-full bg-[#111827] overflow-auto'>
-      <div className='mx-auto max-w-screen-xl h-full w-full'>{children}</div>
-    </main>
+    <>
+      <main className='h-full bg-[#111827] overflow-auto'>
+        <StickyBar />
+        <div className='mx-auto max-w-screen-xl h-full w-full'>{children}</div>
+      </main>
+    </>
   );
 };
 
---
+++ components/sticky-bar.tsx
@@ -0,0 +1,9 @@
+const StickyBar = () => {
+  return (
+    <div className='w-full bg-purple-400 flex justify-center items-center text-white p-1'>
+      <strong>WARNING:</strong>This application is for demo purpose only
+    </div>
+  );
+};
+
+export default StickyBar;
